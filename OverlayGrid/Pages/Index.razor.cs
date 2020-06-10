d6807b6720329077a5d8d2a5d74d561c316294b6
Marijn Pessers <marijn@pessers.net>
2020-06-11 00:19:25 +0200

    Sets a snackbar message --> Bug: Snackbar is not updated (waiting on a MatBlazor update)

--- OverlayGrid/Pages/Index.razor.cs
+++ OverlayGrid/Pages/Index.razor.cs
@@ -1,13 +1,49 @@
-﻿namespace OverlayGrid.Pages
+﻿using MatBlazor;
+using OverlayGrid.Shared;
+using System;
+
+namespace OverlayGrid.Pages
 {
     public partial class Index
     {
         private int Height { get; set; } = 200;
         private int Width { get; set; } = 200;
 
+        private string ErrorMessage { get; set; }
+
+        private SnackBar _snackBar { get; set; }
+
         private void DrawCanvas()
         {
             StateHasChanged();
+        }
+
+
+        async System.Threading.Tasks.Task UploadImageAsync(IMatFileUploadEntry[] files)
+        {
+            try
+            {
+                foreach (var file in files)
+                {
+                    if (!file.Type.StartsWith("image/"))
+                    {
+                        _snackBar.ShowSnackBar("Not a valid image file. Try a different file!");
+                    }
+                    else
+                    {
+                        _snackBar.ShowSnackBar("Image file uploaded!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _snackBar.ShowSnackBar("Something went wrong. Try it again!");
+            }
+            finally
+            {
+                await InvokeAsync(() => { StateHasChanged(); });
+            }
+
         }
     }
 }
---
+++ OverlayGrid/Shared/SnackBar.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OverlayGrid.Shared
+{
+    public partial class SnackBar
+    {
+        [Parameter]
+        public bool IsOpen { get; set; }
+
+        [Parameter]
+        public string Message { get; set; }
+
+        public void ShowSnackBar(string message)
+        {
+            Message = message;
+            IsOpen = true;
+            StateHasChanged();
+        }
+
+        void Close()
+        {
+            IsOpen = false;
+            StateHasChanged();
+        }
+    }
+}
