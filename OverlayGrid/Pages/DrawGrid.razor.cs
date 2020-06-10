aa5e9e8ec4d7ad197278c5e1c93de171ec32da53
Marijn Pessers <marijn@pessers.net>
2020-06-10 22:41:57 +0200

    Add project files.

---
+++ OverlayGrid/Pages/DrawGrid.razor.cs
@@ -0,0 +1,16 @@
+﻿using Blazor.Extensions.Canvas;
+using Microsoft.AspNetCore.Components;
+
+namespace OverlayGrid.Pages
+{
+    public partial class DrawGrid
+    {
+        private BECanvas _canvasReference;
+
+        [Parameter]
+        public int Height { get; set; }
+
+        [Parameter]
+        public int Width { get; set; }
+    }
+}
---
+++ OverlayGrid/Pages/Index.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace OverlayGrid.Pages
+{
+    public partial class Index
+    {
+        private int Height { get; set; } = 200;
+        private int Width { get; set; } = 200;
+
+        private void DrawCanvas()
+        {
+            StateHasChanged();
+        }
+    }
+}
